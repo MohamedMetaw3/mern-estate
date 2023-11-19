7116b4832b16f004e0e8a46eb5547f2223d887a7
Mohamed Metawa <118294479+MohamedMetaw3@users.noreply.github.com>
2023-11-19 17:11:24 +0300

    sign in and token function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bycrptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // sign up function ---------------->
 export const signup = async (req, res, next) => {
@@ -17,6 +19,37 @@ export const signup = async (req, res, next) => {
       newUser,
     });
     // handle error come from our Middleware
+  } catch (error) {
+    next(error);
+  }
+};
+
+// sign in function ---------------->
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User?.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const isMatchPassword = bycrptjs.compareSync(password, validUser?.password);
+    if (!isMatchPassword) {
+      return res.status(401).json({
+        message: "Password is incorrect",
+      });
+    }
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      validUser,
+    });
   } catch (error) {
     next(error);
   }
