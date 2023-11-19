ad5499506c0db19bcc5f16b8439d1b1c2b0b38b2
Mohamed Metawa <118294479+MohamedMetaw3@users.noreply.github.com>
2023-11-19 10:06:20 +0300

    error hanseler Middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,23 @@
 import User from "../models/user.model.js";
 import bycrptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+// sign up function ---------------->
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  //  hash password by bcyrptjs
   const salt = await bycrptjs.genSalt(10);
   const hashedPassword = bycrptjs.hashSync(password, salt);
-
+  // create new user
   const newUser = new User({ username, email, password: hashedPassword });
+  // handle success
   try {
     await newUser.save();
     res.status(201).json({
       message: "User created successfully",
       newUser,
     });
+    // handle error come from our Middleware
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -1,23 +1,39 @@
-import express from "express"; 
-import mongoose  from "mongoose";
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Connect success to mongodb")
-}).catch(()=>{
-    console.log(error)
-});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connect success to mongodb");
+  })
+  .catch(() => {
+    console.log(error);
+  });
 const app = express();
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.get("/",(req,res) => {
-    res.send("Hello World")
-})
+// Error Middleware Handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+ return res.status(statusCode).json({
+  success:false,
+  message,
+  statusCode,
+ })
+});
 
-
-app.listen(3000,() => {
-    console.log("Server is running  on 3000" )})
-
-    // Mo5kzVjrVBxJosok
+// app port listening
+app.listen(3000, () => {
+  console.log("Server is running  on 3000");
+});
